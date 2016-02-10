97774c7854583dd0ab98d99bbad20b4d2f7d5970
clfchris <clfchris@gmail.com>
2016-02-10 09:33:02 -0700

    reload week 4 part 1

--- build/apps/resume/react/components/about.js
+++ build/apps/resume/react/components/about.js
@@ -2,13 +2,23 @@ MyComponents.About = React.createClass({
 
   render: function() {
     return (
-      <div className="card">
-        <div className="card-content">
-        TODO: This is a component to display the basic
-        information about myself such as name, profile photo,
-        major, birth place, ... etc.
+      <div className="card red darken-4">
+        <div className="card-content white-text">
+		  <div className="row">
+		    <div className="col s3">
+		      <img class="materialboxed" width="100" src ="profile.jpg"></img>
+            </div>
+        <div className="col s7">
+          <ul>
+            <li><b>Name: </b> Chris Fanning </li>
+            <li><b>Major: </b> BA in Computer Science </li>
+            <li><b>Hometown: </b> Aurora, Colorado </li>
+            <li><b>Git Link: </b><a href="https://github.com/clfchris">www.github.com/clfchris</a></li>
+         </ul>
+         </div>   
         </div>
       </div>
+    </div>  
     );
   }
 
